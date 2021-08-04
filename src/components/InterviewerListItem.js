ed9fe383c231b48b502d90092a03b08d705af0ef
fictionalparakeets <banksmatthew32@gmail.com>
2021-08-04 17:12:40 -0400

    add interviewerListItem and interviewerList components -incomplete state

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,31 @@
+import React from "react";
+import classNames from 'classnames/bind';
+
+import "components/InterviewerListItem.scss";
+
+/* --------- props:
+    id:number - the id of the interviewer
+    name:string - the name of the interviewer
+    avatar:url - a url to an image of the interviewer
+    selected:boolean - to determine if an interview is selected or not
+    setInterviewer:function - sets the interviewer upon selection
+
+*/
+
+export default function InterviewerListItem(props) {
+  const { id, name, avatar, selected, setInterviewer } = props;
+
+  const InterviewerItemClass = classNames('interviewers__item', {
+    'interviewers__item--selected': selected,
+  });
+  
+  return (
+    <li className={InterviewerItemClass}>
+      <img className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {selected && name}
+    </li>
+  );
+}
--- stories/index.js
+++ stories/index.js
@@ -8,23 +8,40 @@ import "index.scss";
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
 import DayList from "components/DayList";
+import InterviewerListItem from "components/InterviewerListItem";
+import InterviewerList from "components/InterviewerItem";
 
+// ------------------------------------------------- Button
 storiesOf("Button", module)
-.addParameters({ backgrounds: [{ name: "dark", value: "#222f3e", default: true }] })
-.add("Base", () => <Button>Base</Button>)
-.add("Confirm", () => <Button confirm>Confirm</Button>)
-.add("Danger", () => <Button danger>Cancel</Button>)
-.add("Clickable", () => <Button onClick={action("button-clicked")}>Clickable</Button> )
-.add("Disabled", () => <Button disabled onClick={action("button-clicked")}>Disabled</Button> );
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Base", () => <Button>Base</Button>)
+  .add("Confirm", () => <Button confirm>Confirm</Button>)
+  .add("Danger", () => <Button danger>Cancel</Button>)
+  .add("Clickable", () => (
+    <Button onClick={action("button-clicked")}>Clickable</Button>
+  ))
+  .add("Disabled", () => (
+    <Button disabled onClick={action("button-clicked")}>
+      Disabled
+    </Button>
+  ));
 
-
+// ------------------------------------------------- DayListItem
 storiesOf("DayListItem", module)
-.addParameters({ backgrounds: [{ name: "dark", value: "#222f3e", default: true }] })
-.add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
-.add("Selected", () => <DayListItem name="Monday" spots={5} selected />) 
-.add("Full", () => <DayListItem name="Monday" spots={0} />)
-.add("Clickable", () => <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> ); // action() allows us to create a callback that appears in the actions panel when clicked
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Unselected", () => <DayListItem name="Monday" spots={5} />) // To define our stories, we call add() once for each of our test states to generate a story
+  .add("Selected", () => <DayListItem name="Monday" spots={5} selected />)
+  .add("Full", () => <DayListItem name="Monday" spots={0} />)
+  .add("Clickable", () => (
+    <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} />
+  )); // action() allows us to create a callback that appears in the actions panel when clicked
 
+// ------------------------------------------------- DayList
+// Hard-coded Data to be replaced by API:
 const days = [
   {
     id: 1,
@@ -54,5 +71,67 @@ storiesOf("DayList", module)
     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
   ));
 
+// ------------------------------------------------- InterviewerListItem
+// Hard-coded Data to be replaced by API:
+const interviewer = {
+  id: 1,
+  name: "Sylvia Palmer",
+  avatar: "https://i.imgur.com/LpaY82x.png",
+};
 
+storiesOf("InterviewerListItem", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+  })
+  .add("Unselected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+    />
+  ))
+  .add("Selected", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      selected
+    />
+  ))
+  .add("Clickable", () => (
+    <InterviewerListItem
+      id={interviewer.id}
+      name={interviewer.name}
+      avatar={interviewer.avatar}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
+
+// ------------------------------------------------- InterviewerItem
+// Hard-coded Data to be replaced by API:
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+storiesOf("InterviewerList", module)
+  .addParameters({
+    backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+  })
+  .add("Initial", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      setInterviewer={action("setInterviewer")}
+    />
+  ))
+  .add("Preselected", () => (
+    <InterviewerList
+      interviewers={interviewers}
+      interviewer={3}
+      setInterviewer={action("setInterviewer")}
+    />
+  ));
 
